1680f5b5a309113a9ee18c8c963ccb8cc4928385
newtondotcom <asphalt9fr@gmail.com>
2025-02-21 18:08:39 +0100

    fix: change subtitle

--- src/components/slides/intro.tsx
+++ src/components/slides/intro.tsx
@@ -8,8 +8,9 @@ function Intro() {
         <div className="z-20 font-bold tracking-tight">
           <Logo className="text-center text-3xl md:text-6xl lg:text-9xl font-sans py-2 md:py-10" />
         </div>
-        <p className="max-w-xl mx-auto text-sm md:text-xl text-neutral-700 dark:text-neutral-400 text-center font-semibold">
-          Discover the best OS distribution for your needs.
+        <p className="max-w-xl mx-auto text-sm md:text-xl text-neutral-700 text-center font-semibold">
+          Discover the best open source Operating System Ecosystem for your
+          company
         </p>
       </BackgroundLines>
     </div>
