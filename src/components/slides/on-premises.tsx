719dd94ad5e5db52515b4571de63392920efd045
newtondotcom <asphalt9fr@gmail.com>
2025-03-05 19:20:04 +0100

    feat: slide describing the on premises architecture

--- src/components/slides/on-premises.tsx
+++ src/components/slides/on-premises.tsx
@@ -1,13 +1,8 @@
-import Heading from "@/components/others/heading";
-
-function Infra() {
+function OnPremises() {
   const imgUrl = new URL("../../assets/plan.png", import.meta.url).href;
   return (
     <div>
       <div className="flex items-center justify-center w-full flex-col px-2 space-y-4">
-        <div className="font-bold tracking-tight">
-          <Heading content="Architecture Plan" className="py-2" />
-        </div>
         <div className="flex flex-row justify-between space-x-10">
           <img src={imgUrl} alt="Infrastructure Plan" />
         </div>
@@ -16,4 +11,4 @@ function Infra() {
   );
 }
 
-export default Infra;
+export default OnPremises;
