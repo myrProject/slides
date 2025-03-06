565db249ade997744ae1634484d6e2641bc67caf
newtondotcom <asphalt9fr@gmail.com>
2025-03-06 11:32:50 +0100

    feat: slide to describe problems encoutered and openings

---
+++ src/components/slides/pb-openings.tsx
@@ -0,0 +1,19 @@
+import Heading from "@/components/others/heading";
+
+function PbOpenings() {
+  return (
+    <div>
+      <div className="flex items-center justify-center w-full flex-col px-2 space-y-4">
+        <div className="font-bold tracking-tight">
+          <Heading content="Improvements" className="text-5xl" />
+        </div>
+        <div className="flex flex-row justify-between space-x-10">
+          <Heading content="Problems faced" className="text-5xl" />
+        </div>
+        <div className="text-left px-4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PbOpenings;
