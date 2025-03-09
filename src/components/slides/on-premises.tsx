d5fe4b3e9b32093b8094ea362f3f862368787f87
newtondotcom <asphalt9fr@gmail.com>
2025-03-09 18:14:27 +0100

    style: specify image dimensions

--- src/components/slides/on-premises.tsx
+++ src/components/slides/on-premises.tsx
@@ -4,7 +4,7 @@ function OnPremises() {
     <div>
       <div className="flex items-center justify-center w-full flex-col px-2 space-y-4">
         <div className="flex flex-row justify-between space-x-10">
-          <img src={imgUrl} alt="OnPremises Plan" />
+          <img src={imgUrl} alt="OnPremises Plan" width={1400} height={900} />
         </div>
       </div>
     </div>
