1b3d4b23421b64122cc16b6a6e9c208d9f69b6a2
newtondotcom <asphalt9fr@gmail.com>
2025-03-09 18:14:54 +0100

    fix: improve readibility

--- src/components/slides/cloud.tsx
+++ src/components/slides/cloud.tsx
@@ -1,29 +1,40 @@
 import Heading from "@/components/others/heading";
 import GhStats from "@/components/others/gh-stats";
+import { Separator } from "@/components/ui/separator";
 
 function Cloud() {
   return (
     <div>
       <div className="flex items-center justify-center w-full flex-col px-2 space-y-4">
         <div className="font-bold tracking-tight">
-          <Heading content="Cloud Choices" className="py-2" />
+          <Heading content="Architecture Choices" className="py-2" />
         </div>
         <div className="flex flex-row justify-between space-x-10">
-          <div id="k8s" className="flex flex-row space-x-3">
+          <div
+            id="talos-omni"
+            className="flex flex-col space-y-3 justify-center"
+          >
+            <GhStats username="siderolabs" repo="talos" />
+            <GhStats username="siderolabs" repo="omni" />
+          </div>
+
+          <Separator orientation="vertical" className="mx-4 h-full" />
+          <div
+            id="k8s"
+            className="flex flex-row space-x-3 justify-center items-center"
+          >
             <div className="flex flex-col space-y-3">
               <GhStats username="kubernetes" repo="kubernetes" />
-              <ul className="text-xl text-left list-disc">
-                <li>Based on Linux</li>
-                <li>Reproducible package management</li>
-                <li>Declarative configuration</li>
-                <li>Isolated environments</li>
+              <ul className="text-xl text-center">
+                <li>Micro Services Oriented</li>
+                <li>Container Orchestration</li>
+                <li>Declarative Configuration</li>
+                <li>High Availability</li>
+                <li>Scalability</li>
+                <li>Portability</li>
               </ul>
             </div>
             <img src="https://i.redd.it/2gzc189byt921.png" className="w-1/2" />
-          </div>
-          <div id="talos-omni" className="flex flex-col space-y-3">
-            <GhStats username="siderolabs" repo="talos" />
-            <GhStats username="siderolabs" repo="omni" />
           </div>
         </div>
         <div className="text-left px-4"></div>
